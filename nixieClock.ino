74f69ae281fb360ae77c26fe00a9407893950ed2
Nathan <nsafran1217@gmail.com>
2020-04-03 23:09:24 -0400

    still working on transition modes

--- nixieClock.ino
+++ nixieClock.ino
@@ -6,39 +6,19 @@
 #define LATCHPIN 2
 #define DATAPIN 3
 #define CLOCKPIN 4
-#define DOWNBUTTONPIN 12
-#define UPBUTTONPIN 11
-#define MODEBUTTONPIN 10
-#define ROTCLKPIN 9
-#define ROTDTPIN 8
-#define ROTBTTNPIN 7
+//#define DOWNBUTTONPIN 12
+//#define UPBUTTONPIN 11
+//#define MODEBUTTONPIN 10
+#define ROTCLKPIN 10
+#define ROTDTPIN 9
+#define ROTBTTNPIN 8
 
 //EEPROM ADDRESSES
-#define HOURMODEADDRESS 0
+#define twelveHourModeADDRESS 0
 #define ROTATETIMEADDRESS 1
+#define TRANSMODEADDRESS 2
 
 RTC_DS3231 rtc;
-void setup()
-{
-  Serial.begin(57600); //DEBUG
-
-  rtc.begin();
-  pinMode(LATCHPIN, OUTPUT);
-  pinMode(CLOCKPIN, OUTPUT);
-  pinMode(DATAPIN, OUTPUT);
-  pinMode(DOWNBUTTONPIN, INPUT);
-  pinMode(UPBUTTONPIN, INPUT);
-  pinMode(MODEBUTTONPIN, INPUT);
-  digitalWrite(DOWNBUTTONPIN, HIGH);
-  digitalWrite(UPBUTTONPIN, HIGH);
-  digitalWrite(MODEBUTTONPIN, HIGH);
-
-  pinMode(ROTCLKPIN, INPUT);
-  pinMode(ROTDTPIN, INPUT);
-  pinMode(ROTBTTNPIN, INPUT);
-
-  lastStateCLK = digitalRead(ROTCLKPIN);
-}
 
 int hour, minute, second, year, month, day;
 int colons;
@@ -47,25 +27,43 @@ int currentStateCLK;
 int lastStateCLK;
 unsigned long lastButtonPress = 0;
 
-int rotatespeed = EEPROM.read(ROTATETIMEADDRESS);  //time in seconds, min 4, max 59
-boolean HourMode = EEPROM.read(ROTATETIMEADDRESS); //0 for 12, 1 for 24
+int rotatespeed = EEPROM.read(ROTATETIMEADDRESS);            //time in seconds, min 4, max 59
+boolean twelveHourMode = EEPROM.read(twelveHourModeADDRESS); //0 for 12, 1 for 24
+int TransMode = EEPROM.read(TRANSMODEADDRESS);
 boolean dateOrTime = false;
 unsigned long lastBlinkTime = 0;
-unsigned long blinkOffTime = 200;
-unsigned long blinkOnTIme = 800;
-int digitMod = 0;
+unsigned long blinkTime = 500;
+unsigned long waitForDisplay = 0; //for displaying mode
 int nextSwitch = 0;
-int mode = 0;
-
+int displayMode = 0;
 //0  Time
 //1  Date
 //2  Rotate time and date
-//3  Set time
-//4  Set date
-//5  Set 12 or 24 hour
-//6  Set rotation speed in secs
 
-int readRotEncoder(int counter)
+void setup()
+{
+  Serial.begin(57600); //DEBUG
+
+  rtc.begin();
+  pinMode(LATCHPIN, OUTPUT);
+  pinMode(CLOCKPIN, OUTPUT);
+  pinMode(DATAPIN, OUTPUT);
+  //pinMode(DOWNBUTTONPIN, INPUT);
+  //pinMode(UPBUTTONPIN, INPUT);
+  //pinMode(MODEBUTTONPIN, INPUT);
+  //digitalWrite(DOWNBUTTONPIN, HIGH);
+  //digitalWrite(UPBUTTONPIN, HIGH);
+  //digitalWrite(MODEBUTTONPIN, HIGH);
+
+  pinMode(ROTCLKPIN, INPUT);
+  pinMode(ROTDTPIN, INPUT);
+  pinMode(ROTBTTNPIN, INPUT);
+  digitalWrite(ROTBTTNPIN, HIGH);
+
+  lastStateCLK = digitalRead(ROTCLKPIN);
+}
+
+int readRotEncoder(int counter) //takes a counter in and spits it back out if it changes
 {
   // Read the current state of CLK
   currentStateCLK = digitalRead(ROTCLKPIN);
@@ -88,7 +86,7 @@ int readRotEncoder(int counter)
   lastStateCLK = currentStateCLK;
   return counter;
 }
-boolean readRotButton()
+boolean readRotButton() //true if button pressed
 {
   // Read the button state
   int btnState = digitalRead(ROTBTTNPIN);
@@ -109,46 +107,59 @@ boolean readRotButton()
     }
     // Remember last button press event
   }
-  delay(1);
+  else
+  {
+    return false;
+  }
+}
+
+int changeMode(int mode, int numOfModes) //numofmodes starts at 0! display mode on tube, easy for selecting
+{
+  int lastmode = mode;
+  mode = readRotEncoder(mode);
+  if (mode > 2) //if mode invalid, loop around
+  {
+    mode = 0;
+  }
+  else if (mode < 0)
+  {
+    mode = 2;
+  }
+  if (mode != lastmode)
+  {
+    waitForDisplay = millis();
+    while (millis() - waitForDisplay < 500)
+    {
+      delay(10);
+      writeToNixie(mode, 255, 255, 0);
+      mode = changeMode(mode, numOfModes);
+    }
+  }
+  return mode;
 }
 void loop() //bro change all this to rotate with up down buttons. option should edit the settings
 {
   delay(10);
-  if (digitalRead(UPBUTTONPIN) == LOW)
+  if (readRotButton()) //enter set menu
   {
-    delay(200); //fake button debounce
-    mode++;
-    if (mode > 2)
-    {
-      mode = 0;
-    }
+    settingsMenu();
   }
-  if (digitalRead(DOWNBUTTONPIN) == LOW)
-  {
-    delay(200); //fake button debounce
-    mode = mode - 1;
-    if (mode < 0)
-    {
-      mode = 0;
-    }
-  }
-  switch (mode)
+  displayMode = changeMode(displayMode, 2);
+  switch (displayMode)
   {
   case 0:
-    timeMode();                            //add 12/24h modes
-    if (digitalRead(MODEBUTTONPIN) == LOW) //enter set menu
+    if (TransMode == 0)
     {
-      delay(200); //fake button debounce
-      setTime();
+      normalTimeMode();
     }
+    else if (TransMode == 1)
+    {
+      scrollTimeMode();
+    }
+
     break;
   case 1:
     dateMode();
-    if (digitalRead(MODEBUTTONPIN) == LOW) //enter set menu
-    {
-      delay(200); //fake button debounce
-      setDate();
-    }
     break;
   case 2:
     rotateMode();
@@ -156,23 +167,118 @@ void loop() //bro change all this to rotate with up down buttons. option should 
   }
 }
 
-void timeMode()
+void settingsMenu()
+{
+  int settingsMode = 0;
+  boolean exitSettings = false;
+  while (!exitSettings)
+  {
+    delay(10);
+    settingsMode = readRotEncoder(settingsMode);
+    writeToNixie(settingsMode, 255, 255, 0);
+    if (readRotButton())
+    {
+      switch (settingsMode)
+      {
+      case 0:
+        exitSettings = true; //exit back to display mode
+        break;
+      case 1:
+        setTime();
+        break;
+      case 2:
+        setDate();
+        break;
+      case 3:
+        setRotationSpeed();
+        break;
+      case 4:
+        setHourDisplay();
+        break;
+      case 5:
+        setTransitionMode();
+        break;
+      }
+    }
+    if (settingsMode > 5)
+    {
+      settingsMode = 0;
+    }
+    if (settingsMode < 0)
+    {
+      settingsMode = 5;
+    }
+  }
+}
+void normalTimeMode()
 {
   getDateTime();
-
-  //printtime(); //debug
-
   if (second % 2 == 0) //too slow, change to millis timer for 500 milliseconds
   {
-    colons = 16;
+    colons = 15;
   }
   else
   {
     colons = 0;
   }
+  if (twelveHourMode)
+  {
+    if (hour > 12)
+    {
+      writeToNixie(hour - 12, minute, second, colons);
+    }
+    else if (hour == 00)
+    {
+      writeToNixie(12, minute, second, colons);
+    }
 
-  writeToNixie(hour, minute, second, colons);
+    else
+    {
+      writeToNixie(hour, minute, second, colons);
+    }
+  }
+  else
+  {
+    writeToNixie(hour, minute, second, colons);
+  }
 }
+
+void scrollTimeMode()
+{
+    getDateTime();
+  if (second % 2 == 0) //too slow, change to millis timer for 500 milliseconds
+  {
+    colons = 15;
+  }
+  else
+  {
+    colons = 0;
+  }
+  if (twelveHourMode)
+  {
+    if (hour > 12)
+    {
+      writeToNixie(hour - 12, minute, second, colons);
+    }
+    else if (hour == 00)
+    {
+      writeToNixie(12, minute, second, colons);
+    }
+
+    else
+    {
+      writeToNixie(hour, minute, second, colons);
+    }
+  }
+  else
+  {
+    writeToNixie(hour, minute, second, colons);
+  }
+  int lasthour = hour;
+  int lastminute =  minute;
+  int lastsecond = second;
+}
+
 void dateMode()
 {
   getDateTime();
@@ -180,7 +286,7 @@ void dateMode()
   //printtime(); //debug
   if (second % 2 == 0) //too slow, change to millis timer for 500 milliseconds
   {
-    colons = 10;
+    colons = 6;
   }
   else
   {
@@ -190,11 +296,6 @@ void dateMode()
 }
 void rotateMode()
 {
-  if (digitalRead(MODEBUTTONPIN) == LOW)
-  {
-    delay(200); //fake button debounce
-    setRotationSpeed();
-  }
   if (second == nextSwitch)
   {
     nextSwitch = ((second + rotatespeed) % 60);
@@ -202,7 +303,7 @@ void rotateMode()
   }
   if (dateOrTime)
   {
-    timeMode();
+    normalTimeMode(); /////////////////maybe change
   }
   else
   {
@@ -211,128 +312,221 @@ void rotateMode()
 }
 void setTime()
 {
-  digitMod = 0;
-  while (digitMod < 4)
+  int lasthour, lastminute, lastsecond;
+  int digitMod = 0;
+  lastBlinkTime = millis();
+  while (digitMod < 3)
   {
-    delay(50);
-    //printtime(); //debug
-    if (digitalRead(MODEBUTTONPIN) == LOW)
+    lasthour = hour;
+    lastminute = minute;
+    lastsecond = second;
+    delay(5);
+    if (readRotButton())
     {
       lastBlinkTime = millis(); //reset timer
-      delay(200);               //fake button debounce
       digitMod++;
     }
-    if (digitMod == 3)
+    if ((millis() - lastBlinkTime) < blinkTime)
     {
-      setHourDisplay();
-      delay(200); //fake button debounce
-      digitMod++;
+
+      writeToNixie(hour, minute, second, 15);
     }
-    if ((millis() - lastBlinkTime) > blinkOnTIme)
+    else if (((millis() - lastBlinkTime) > blinkTime * 2))
+    {
+      writeToNixie(hour, minute, second, 15);
+      lastBlinkTime = millis(); //reset timer
+    }
+    else
     {
       //switch off
       switch (digitMod)
       {
       case 0:
-        writeToNixie(255, minute, second, 16); //255 will blank the display
+        writeToNixie(255, minute, second, 15); //255 will blank the display
         break;
       case 1:
-        writeToNixie(hour, 255, second, 16);
+        writeToNixie(hour, 255, second, 15);
         break;
       case 2:
-        writeToNixie(hour, minute, 255, 16);
+        writeToNixie(hour, minute, 255, 15);
         break;
       }
-      if ((millis() - lastBlinkTime) > blinkOnTIme + blinkOffTime)
+    }
+
+    switch (digitMod)
+    {
+    case 0: //////////////////////////////////////////////////////////down is broken
+      hour = readRotEncoder(hour);
+      if (hour != lasthour)
       {
         lastBlinkTime = millis(); //reset timer
-        writeToNixie(hour, minute, second, 16);
-      }
-    }
-    if (digitalRead(UPBUTTONPIN) == LOW)
-    {
-      delay(200);               //fake button debounce
-      lastBlinkTime = millis(); //reset timer
-      writeToNixie(hour, minute, second, 16);
-      switch (digitMod)
-      {
-      case 0:
-        hour++;
         if (hour > 23)
         {
           hour = 0;
         }
-        break;
-      case 1:
-        minute++;
-
+        else if (hour < 0)
+        {
+          hour = 23;
+        }
+      }
+      break;
+    case 1:
+      minute = readRotEncoder(minute);
+      if (minute != lastminute)
+      {
+        lastBlinkTime = millis(); //reset timer
         if (minute > 59)
         {
           minute = 0;
         }
-        break;
-      case 2:
-        second++;
+        else if (minute < 0)
+        {
+          minute = 59;
+        }
+      }
+      break;
+    case 2:
+      second = readRotEncoder(second);
+      if (second != lastsecond)
+      {
+        lastBlinkTime = millis(); //reset timer
         if (second > 59)
         {
           second = 0;
         }
-        break;
-      }
-    }
-    if (digitalRead(DOWNBUTTONPIN) == LOW)
-    {
-      delay(200);               //fake button debounce
-      lastBlinkTime = millis(); //reset timer
-      writeToNixie(hour, minute, second, 16);
-      switch (digitMod)
-      {
-      case 0:
-        hour = hour - 1;
-        if (hour < 0)
-        {
-          hour = 23;
-        }
-        break;
-      case 1:
-        minute = minute - 1;
-        if (minute < 0)
-        {
-          minute = 59;
-        }
-        break;
-      case 2:
-        second = second - 1;
-        if (second < 0)
+        else if (second < 0)
         {
           second = 59;
         }
-        break;
       }
+      break;
     }
   }
   rtc.adjust(DateTime(year, month, day, hour, minute, second));
 }
 void setDate()
 {
+  int lastyear, lastmonth, lastday;
+  int digitMod = 0;
+  lastBlinkTime = millis();
+  while (digitMod < 3)
+  {
+    lastyear = year;
+    lastmonth = month;
+    lastday = day;
+    delay(5);
+    if (readRotButton())
+    {
+      lastBlinkTime = millis(); //reset timer
+      digitMod++;
+    }
+    if (digitMod == 2)
+    {
+      writeToNixie(255, year / 100, year % 100, 6);
+    }
+    else
+    {
+      if ((millis() - lastBlinkTime) < blinkTime)
+      {
+
+        writeToNixie(month, day, year, 6);
+      }
+      else if (((millis() - lastBlinkTime) > blinkTime * 2))
+      {
+        writeToNixie(month, day, year, 6);
+        lastBlinkTime = millis(); //reset timer
+      }
+      else
+      {
+        //switch off
+        switch (digitMod)
+        {
+        case 0:
+          writeToNixie(255, day, year, 6); //255 will blank the display
+          break;
+        case 1:
+          writeToNixie(month, 255, year, 6);
+          break;
+        }
+      }
+    }
+
+    switch (digitMod)
+    {
+    case 0:
+      month = readRotEncoder(month);
+      if (month != lastmonth)
+      {
+        lastBlinkTime = millis(); //reset timer
+        if (month > 12)
+        {
+          month = 1;
+        }
+        else if (month < 1)
+        {
+          month = 12;
+        }
+      }
+      break;
+    case 1:
+      day = readRotEncoder(day);
+      if (day != lastday)
+      {
+        lastBlinkTime = millis(); //reset timer
+        if (day > 31)
+        {
+          day = 1;
+        }
+        else if (day < 1)
+        {
+          day = 31;
+        }
+      }
+      break;
+    case 2:
+      year = readRotEncoder(year);
+      if (year != lastyear)
+      {
+        lastBlinkTime = millis(); //reset timer
+        if (year > 2100)
+        {
+          year = 2100;
+        }
+        else if (year < 2020)
+        {
+          year = 2020;
+        }
+      }
+      break;
+    }
+  }
+  DateTime now = rtc.now();
+  hour = now.hour();
+  minute = now.minute();
+  second = now.second();
+  rtc.adjust(DateTime(year, month, day, hour, minute, second));
 }
 void setHourDisplay()
 {
-  boolean origBool = HourMode;
-  while (digitalRead(MODEBUTTONPIN) != LOW)
+  boolean origBool = twelveHourMode;
+  int counter = 0;
+  if (twelveHourMode)
   {
-    delay(100);
-    if (digitalRead(UPBUTTONPIN) == LOW)
+    counter = 1;
+  }
+  while (!readRotButton())
+  {
+    delay(10);
+    counter = readRotEncoder(counter);
+    if (counter % 2 == 0)
     {
-      delay(200);
-      HourMode = !HourMode;
+      twelveHourMode = false;
     }
-    if (digitalRead(DOWNBUTTONPIN) == LOW)
+    else
     {
-      delay(200);
-      HourMode = !HourMode;
+      twelveHourMode = true;
     }
-    if (HourMode)
+    if (twelveHourMode)
     {
       writeToNixie(12, 255, 255, 0);
     }
@@ -341,45 +535,52 @@ void setHourDisplay()
       writeToNixie(24, 255, 255, 0);
     }
   }
-  if (origBool != HourMode) //thi sis not working
+  if (origBool != twelveHourMode) //thi sis not working
   {
-    EEPROM.write(HOURMODEADDRESS, (boolean)HourMode); //commmit change to EEPROM
-    delay(500);
+    EEPROM.write(twelveHourModeADDRESS, (boolean)twelveHourMode); //commmit change to EEPROM
   }
 }
 void setRotationSpeed()
 {
   int origRotateSpeed = rotatespeed;
-  while (digitalRead(MODEBUTTONPIN) != LOW)
+  while (!readRotButton())
   {
-    delay(100);
-    if (digitalRead(UPBUTTONPIN) == LOW)
+    delay(10);
+    rotatespeed = readRotEncoder(rotatespeed);
+    if (rotatespeed > 59)
     {
-      delay(200);
-      rotatespeed++;
-      if (rotatespeed > 59)
-      {
-        rotatespeed = 59;
-      }
-    }
-    if (digitalRead(DOWNBUTTONPIN) == LOW)
-    {
-      delay(200);
-      rotatespeed = rotatespeed - 1;
-      if (rotatespeed < 2)
-      {
-        rotatespeed = 2;
-      }
+      rotatespeed = 59;
     }
     writeToNixie(255, 255, rotatespeed, 0);
   }
   if (origRotateSpeed != rotatespeed)
   {
     EEPROM.write(ROTATETIMEADDRESS, rotatespeed); //commit change to EEPROM
-    delay(500);
   }
 }
 
+void setTransitionMode()
+{
+  int origTransMode = TransMode;
+  while (!readRotButton())
+  {
+    delay(10);
+    TransMode = readRotEncoder(TransMode);
+    if (TransMode > 1)
+    {
+      TransMode = 0;
+    }
+    if (TransMode < 0)
+    {
+      TransMode = 1;
+    }
+    writeToNixie(255, 255, TransMode, 0);
+  }
+  if (origTransMode != TransMode)
+  {
+    EEPROM.write(TRANSMODEADDRESS, TransMode); //commit change to EEPROM
+  }
+}
 void getDateTime()
 {
   DateTime now = rtc.now();
@@ -395,10 +596,10 @@ void writeToNixie(int hours, int minutes, int seconds, int divider) //write data
 {
   printNixieToSerial(hours, minutes, seconds, divider);
   digitalWrite(LATCHPIN, LOW);
-  shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, convertToNixe(hours));
-  shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, convertToNixe(minutes));
-  shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, convertToNixe(seconds));
   shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, divider);
+  shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, convertToNixe(seconds));
+  shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, convertToNixe(minutes));
+  shiftOut(DATAPIN, CLOCKPIN, MSBFIRST, convertToNixe(hours));
   digitalWrite(LATCHPIN, HIGH);
 }
 
@@ -427,7 +628,7 @@ void printNixieToSerial(int hours, int minutes, int seconds, int divider)
   Serial.print('-');
   Serial.print(divider, DEC);
   Serial.print('-');
-  Serial.print(mode, DEC);
+  Serial.print(displayMode, DEC);
   Serial.println();
 }
 void printtime()
